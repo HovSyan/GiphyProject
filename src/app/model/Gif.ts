4754f502d5dab0dc0a13f756f9883d64754f711f
hovhannes.s.sahakyan <hovhannes.s.sahakyan@synisys.com>
2019-11-07 18:11:55 +0400

    improvement: make gifs renderd in DOM

--- src/app/model/Gif.ts
+++ src/app/model/Gif.ts
@@ -1,17 +1,45 @@
 export class GifObject {
-  constructor(type: string,
-              id: string,
-              slug: string,
-              url: string,
-              bitlyUrl: string,
-              embedUrl: string,
-              username: string,
-              source: string,
-              rating: string,
-              sourceTLD: string,
-              sourcePostURL: string,
-              updateDatetime: string,
-              createDatetime: string,
-              importDatetime: string,
-              title: string) { }
+  constructor(public type: string,
+              public id: string,
+              public slug: string,
+              public url: string,
+              public bitlyUrl: string,
+              public embedUrl: string,
+              public username: string,
+              public source: string,
+              public rating: string,
+              public sourceTLD: string,
+              public sourcePostURL: string,
+              public updateDatetime: string,
+              public createDatetime: string,
+              public importDatetime: string,
+              public title: string) { }
+
+  get bitly_url(): string {
+    return this.bitlyUrl;
+  }
+
+  get embed_url(): string {
+    return this.embedUrl;
+  }
+
+  get source_tld(): string {
+    return this.sourceTLD;
+  }
+
+  get source_post_url(): string {
+    return this.sourcePostURL;
+  }
+
+  get update_datetime(): string {
+    return this.updateDatetime;
+  }
+
+  get create_datetime(): string {
+    return this.createDatetime;
+  }
+
+  get import_datetime(): string {
+    return this.importDatetime;
+  }
 }
