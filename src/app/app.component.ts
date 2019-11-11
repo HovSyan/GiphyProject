aa09e048b9ff9bf7a09c3ac6e93a8da01338455a
Hovhannes Sahakyan <syanhovhannes@gmail.com>
2019-11-11 19:09:42 +0400

    improvemnet: add facebook icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent {
   private readonly GIPHY_PAGE_URL = 'https://giphy.com/';
   private readonly GITHUB_PAGE_URL = 'https://github.com/HovSyan';
+  private readonly FACEBOOK_PAGE_URL = 'https://www.facebook.com/HovhanneSahakyan';
 
   onPoweredByGiphyClick() {
     window.location.href = this.GIPHY_PAGE_URL;
@@ -15,5 +16,9 @@ export class AppComponent {
 
   onGithubClick() {
     window.location.href = this.GITHUB_PAGE_URL;
+  }
+
+  onFacebookClick() {
+    window.location.href = this.FACEBOOK_PAGE_URL;
   }
 }
